193d03201d0e25c568efa3dce2361f9a5fa68abf
Flavio Duarte <flduarte13@gmail.com>
2024-10-31 18:39:23 -0300

    Update oh-my-zsh-install.sh

--- oh-my-zsh-install.sh
+++ oh-my-zsh-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Zsh install
 if ! command -v zsh 2>&1 >/dev/null; then
   echo "Installing zsh!"
   apt install zsh
@@ -7,10 +8,19 @@ else
   echo "zsh version: $(zsh --version)"
 fi
 
+# Set zsh as default
 if [ "$SHELL" = "/usr/bin/zsh" ]; then
   echo "zsh is the default shell!"
 else
   echo "Setting zsh as the default shell!"
+  chsh -s "/usr/bin/zsh"
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Oh My Zsh install
+if [ -d "~/.oh-my-zsh" ]
+then
+    echo "Oh My Zsh installed!"
+else
+    echo "Installing Oh My Zsh!"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
