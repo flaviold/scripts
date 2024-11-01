2ba92d3124a30e37ec65a19edacc70aa5c453a4c
Flavio Duarte <flduarte13@gmail.com>
2024-10-31 23:30:53 -0300

    Update lazyvim.sh

--- lazyvim.sh
+++ lazyvim.sh
@@ -3,7 +3,9 @@
 # Neovim install
 if ! command -v nvim 2>&1 >/dev/null; then
   echo "Installing neovim!"
-  sudo apt install neovim
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+  sudo rm -rf /opt/nvim
+  sudo tar -C /opt -xzf nvim-linux64.tar.gz
 else
   echo "neovim version: $(nvim --version)"
 fi
