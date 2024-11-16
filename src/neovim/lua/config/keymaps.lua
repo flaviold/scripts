9e97b22fbf8c8e2129e656bb08e8acd84d01dc84
Flavio Duarte <flduarte13@gmail.com>
2024-11-15 22:50:50 -0300

    Create keymaps.lua

---
+++ src/neovim/lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap
+
+-- Only set this up if we're in a tmux session
+if os.getenv("TMUX") then
+  map.del("n", "<C-h>")
+  map.del("n", "<C-j>")
+  map.del("n", "<C-k>")
+  map.del("n", "<C-l>")
+  map.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
+  map.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>")
+  map.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>")
+  map.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>")
+end
